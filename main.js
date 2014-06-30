518fff71a4f9e0afafd924beb9e34e930b29cd7d
Jacob <jakeboone@aol.com>
2014-06-30 10:35:13 -0600

    end of lesson

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 $(document).on('ready', function() {
 
-
+// part1 stopwatch
 var stopwatch = function(hours, minutes, seconds){
 
 	if(!hours){
 		hours= "00";
 	}
+	// hours=hours || 0:
 	if(!minutes){
 		minutes= "00";
 	}
@@ -13,11 +14,43 @@ var stopwatch = function(hours, minutes, seconds){
 		seconds= "00";
 	}
 
-
 	console.log(hours, minutes, seconds)
-
+	// return hours + ":" + minutes + ":" +
 }
   
 	stopwatch();
+
+//  part 2 getter setter
+
+// var getnset = function(link, element){
+
+// getter : var color = $('.button').css('color')
+// setter: $('.button').css('color', 'red')
+
+
+var href = function(el, url){
+	// if(arguments.length === 2)
+	if(url){
+		el.attr('hreh', url);
+	}
+	else{
+		return el.attr('href')
+	}
+}
+
+//GETTER
+
+var myUrl = href( $('.mylink'));
+
+
+// SETTER
+
+href( $('.mylink'), 'http://google.com' );
+
+
+
+
+
+
 
 });
